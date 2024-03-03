51d55f8eef4d4c54fbefd4f4fc550c3a2262451b
Musaab Abdalla <musabd50118@stud.noroff.no>
2024-03-03 20:56:05 +0100

    solves an issue with auth login fuction

--- src/js/api/auth/login.mjs
+++ src/js/api/auth/login.mjs
@@ -3,6 +3,7 @@ import { API_AUTH, API_BASE, API_LOGIN } from "../../utils/variables.mjs";
 
 export async function userLogin(userEmail, userPassword) {
   const dangerAlert = document.querySelector("#danger-alert")
+  try {
   const option = {
     method: "POST",
     headers: {
@@ -14,23 +15,23 @@ export async function userLogin(userEmail, userPassword) {
     }),
   };
   const response = await fetch(API_BASE + API_AUTH + API_LOGIN, option);
-  const data = (await response.json()).data
-  if (response.ok) {
-    const { accessToken, ...profile } = data;
-    save("token", accessToken);
-    save("profile", profile);
-    window.location.replace("../../../profile");
-    return profile;
-  }
-  else{
-
-    console.log(data.errors[0].message)
+  const  json= (await response.json()).data   
+  const { accessToken, ...profile } = json;
+  save("token", accessToken);
+  save("profile", profile);
+  window.location.replace("./profile/index.html");
+    
+  } 
+  catch (error) {
     dangerAlert.classList.toggle("d-none")
-    dangerAlert.innerHTML= data.errors[0].message
+    dangerAlert.innerHTML= `Invalid Username or Password` 
     setTimeout(()=>{
         dangerAlert.classList.toggle("d-none")
     },5500)     
-    throw new Error("Could not login account");
+    
+    console.log(error)
   }
 
-}
+  }
+
+
